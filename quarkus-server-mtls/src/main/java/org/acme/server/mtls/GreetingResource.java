03e396c0945393837be21d7f8816519b04276b98
Diego Uchôa Neves <diego-uchoa@users.noreply.github.com>
2023-03-07 16:08:56 -0300

    Update GreetingResource.java

--- quarkus-server-mtls/src/main/java/org/acme/server/mtls/GreetingResource.java
+++ quarkus-server-mtls/src/main/java/org/acme/server/mtls/GreetingResource.java
@@ -5,12 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.logging.Logger;
+
 @Path("/hello-server")
 public class GreetingResource {
+
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("Serving the Hello message from server in OpenShift.");
         return "hello from server";
     }
 }
